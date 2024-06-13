a0beccb65d840d67af4cccc9197d816653148917
Zarko Ivkovic <117210785+zarkoivkovicc@users.noreply.github.com>
2024-06-13 16:09:41 +0200

    small update extract.sh comment

--- low_data/extract.sh
+++ low_data/extract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# call this script with "4033 3392" as the argument to reproduce paper results
+# call this script with "4033 3392 939" as the argument to reproduce paper results
 samplings=$(ls -d */)
 for sampling in $samplings
 do
